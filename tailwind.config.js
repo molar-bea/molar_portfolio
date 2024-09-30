163d1c25a05454c46df8f89324e3040786d8af8b
Bea Riña Molar <molarbea123@gmail.com>
2024-09-30 22:28:24 +0800

    amo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        leagueSpartan:['League Spartan', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
